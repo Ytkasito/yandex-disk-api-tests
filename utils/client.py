828403301072d107e7a48cfe2ff2963e1128a82d
Kirill <kira090801@mail.ru>
2026-05-11 01:50:00 +0300

    Add API request and response logging

--- utils/client.py
+++ utils/client.py
@@ -3,6 +3,8 @@
 import requests
 from dotenv import load_dotenv
 
+from utils.logger import logger
+
 
 load_dotenv()
 
@@ -10,7 +12,6 @@ load_dotenv()
 class YandexDiskClient:
 
     def __init__(self):
-
         self.base_url = os.getenv("BASE_URL")
         self.token = os.getenv("TOKEN")
 
@@ -20,12 +21,20 @@ class YandexDiskClient:
             "Content-Type": "application/json"
         }
 
-    def get_disk_info(self):
+    def log_response(self, response):
+        logger.info(f"REQUEST: {response.request.method} {response.url}")
+        logger.info(f"RESPONSE STATUS: {response.status_code}")
+        logger.info(f"RESPONSE BODY: {response.text}")
 
-        return requests.get(
+    def get_disk_info(self):
+        response = requests.get(
             f"{self.base_url}/v1/disk",
             headers=self.headers
         )
+
+        self.log_response(response)
+
+        return response
 
     def create_folder(self, path, fields=None):
         params = {"path": path}
@@ -33,88 +42,16 @@ class YandexDiskClient:
         if fields is not None:
             params["fields"] = fields
 
-        return requests.put(
+        response = requests.put(
             f"{self.base_url}/v1/disk/resources",
             headers=self.headers,
             params=params
         )
 
-    def copy_resource(
-        self,
-        from_path,
-        path,
-        overwrite=None,
-        force_async=None,
-        fields=None
-    ):
-        params = {
-            "from": from_path,
-            "path": path
-        }
+        self.log_response(response)
 
-        if overwrite is not None:
-            params["overwrite"] = str(overwrite).lower()
-        if force_async is not None:
-            params["force_async"] = str(force_async).lower()
-        if fields is not None:
-            params["fields"] = fields
+        return response
 
-        return requests.post(
-            f"{self.base_url}/v1/disk/resources/copy",
-            headers=self.headers,
-            params=params
-        )
-
-    def delete_resource(self, path, permanently=True):
-        return requests.delete(
-            f"{self.base_url}/v1/disk/resources",
-            headers=self.headers,
-            params={
-                "path": path,
-                "permanently": str(permanently).lower()
-            }
-        )
-
-    def restore_from_trash(self, path, name=None, overwrite=False):
-        params = {
-            "path": path,
-            "overwrite": str(overwrite).lower()
-        }
-
-        if name:
-            params["name"] = name
-
-        return requests.put(
-            f"{self.base_url}/v1/disk/trash/resources/restore",
-            headers=self.headers,
-            params=params
-        )
-    
-    def get_resource_metadata(self, path):
-        return requests.get(
-            f"{self.base_url}/v1/disk/resources",
-            headers=self.headers,
-            params={"path": path}
-        )
-    
-    def get_trash_resource_metadata(self, path="/"):
-        return requests.get(
-            f"{self.base_url}/v1/disk/trash/resources",
-            headers=self.headers,
-            params={"path": path}
-        )
-    
-    def move_resource(self, from_path, to_path, overwrite=False):
-        return requests.post(
-            f"{self.base_url}/v1/disk/resources/move",
-            headers=self.headers,
-            params={
-                "from": from_path,
-                "path": to_path,
-                "overwrite": str(overwrite).lower()
-            }
-        )
-    
     def get_resource_metadata(
         self,
         path,
@@ -140,14 +77,111 @@ class YandexDiskClient:
         if sort is not None:
             params["sort"] = sort
 
-        return requests.get(
+        response = requests.get(
             f"{self.base_url}/v1/disk/resources",
             headers=self.headers,
             params=params
         )
-    
+
+        self.log_response(response)
+
+        return response
+
+    def copy_resource(
+        self,
+        from_path,
+        path,
+        overwrite=None,
+        force_async=None,
+        fields=None
+    ):
+        params = {
+            "from": from_path,
+            "path": path
+        }
+
+        if overwrite is not None:
+            params["overwrite"] = str(overwrite).lower()
+        if force_async is not None:
+            params["force_async"] = str(force_async).lower()
+        if fields is not None:
+            params["fields"] = fields
+
+        response = requests.post(
+            f"{self.base_url}/v1/disk/resources/copy",
+            headers=self.headers,
+            params=params
+        )
+
+        self.log_response(response)
+
+        return response
+
     def get_operation_status(self, operation_href):
-        return requests.get(
+        response = requests.get(
             operation_href,
             headers=self.headers
         )
+
+        self.log_response(response)
+
+        return response
+
+    def delete_resource(self, path, permanently=True):
+        response = requests.delete(
+            f"{self.base_url}/v1/disk/resources",
+            headers=self.headers,
+            params={
+                "path": path,
+                "permanently": str(permanently).lower()
+            }
+        )
+
+        self.log_response(response)
+
+        return response
+
+    def get_trash_resource_metadata(self, path="/"):
+        response = requests.get(
+            f"{self.base_url}/v1/disk/trash/resources",
+            headers=self.headers,
+            params={"path": path}
+        )
+
+        self.log_response(response)
+
+        return response
+
+    def restore_from_trash(self, path, name=None, overwrite=False):
+        params = {
+            "path": path,
+            "overwrite": str(overwrite).lower()
+        }
+
+        if name:
+            params["name"] = name
+
+        response = requests.put(
+            f"{self.base_url}/v1/disk/trash/resources/restore",
+            headers=self.headers,
+            params=params
+        )
+
+        self.log_response(response)
+
+        return response
+
+    def move_resource(self, from_path, to_path, overwrite=False):
+        response = requests.post(
+            f"{self.base_url}/v1/disk/resources/move",
+            headers=self.headers,
+            params={
+                "from": from_path,
+                "path": to_path,
+                "overwrite": str(overwrite).lower()
+            }
+        )
+
+        self.log_response(response)
+
+        return response
