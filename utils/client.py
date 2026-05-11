ea71691a9904457e4708fcce12bb8d502061ce3a
Kirill <kira090801@mail.ru>
2026-05-11 13:53:14 +0300

    refactor: replace raw requests with Session, add timeout=10 to all methods

--- utils/client.py
+++ utils/client.py
@@ -1,4 +1,4 @@
-﻿import os
+import os
 
 import requests
 from dotenv import load_dotenv
@@ -8,18 +8,21 @@ from utils.logger import logger
 
 load_dotenv()
 
+DEFAULT_TIMEOUT = 10
+
 
 class YandexDiskClient:
 
     def __init__(self):
         self.base_url = os.getenv("BASE_URL")
-        self.token = os.getenv("TOKEN")
+        self.timeout = DEFAULT_TIMEOUT
 
-        self.headers = {
-            "Authorization": f"OAuth {self.token}",
+        self.session = requests.Session()
+        self.session.headers.update({
+            "Authorization": f"OAuth {os.getenv('TOKEN')}",
             "Accept": "application/json",
             "Content-Type": "application/json"
-        }
+        })
 
     def log_response(self, response):
         logger.info(f"REQUEST: {response.request.method} {response.url}")
@@ -27,29 +30,24 @@ class YandexDiskClient:
         logger.info(f"RESPONSE BODY: {response.text}")
 
     def get_disk_info(self):
-        response = requests.get(
+        response = self.session.get(
             f"{self.base_url}/v1/disk",
-            headers=self.headers
+            timeout=self.timeout
         )
-
         self.log_response(response)
-
         return response
 
     def create_folder(self, path, fields=None):
         params = {"path": path}
-
         if fields is not None:
             params["fields"] = fields
 
-        response = requests.put(
+        response = self.session.put(
             f"{self.base_url}/v1/disk/resources",
-            headers=self.headers,
-            params=params
+            params=params,
+            timeout=self.timeout
         )
-
         self.log_response(response)
-
         return response
 
     def get_resource_metadata(
@@ -77,28 +75,16 @@ class YandexDiskClient:
         if sort is not None:
             params["sort"] = sort
 
-        response = requests.get(
+        response = self.session.get(
             f"{self.base_url}/v1/disk/resources",
-            headers=self.headers,
-            params=params
+            params=params,
+            timeout=self.timeout
         )
-
         self.log_response(response)
-
         return response
 
-    def copy_resource(
-        self,
-        from_path,
-        path,
-        overwrite=None,
-        force_async=None,
-        fields=None
-    ):
-        params = {
-            "from": from_path,
-            "path": path
-        }
+    def copy_resource(self, from_path, path, overwrite=None, force_async=None, fields=None):
+        params = {"from": from_path, "path": path}
 
         if overwrite is not None:
             params["overwrite"] = str(overwrite).lower()
@@ -107,34 +93,23 @@ class YandexDiskClient:
         if fields is not None:
             params["fields"] = fields
 
-        response = requests.post(
+        response = self.session.post(
             f"{self.base_url}/v1/disk/resources/copy",
-            headers=self.headers,
-            params=params
+            params=params,
+            timeout=self.timeout
         )
-
         self.log_response(response)
-
         return response
 
     def get_operation_status(self, operation_href):
-        response = requests.get(
+        response = self.session.get(
             operation_href,
-            headers=self.headers
+            timeout=self.timeout
         )
-
         self.log_response(response)
-
         return response
 
-    def delete_resource(
-        self,
-        path,
-        permanently=None,
-        md5=None,
-        force_async=None,
-        fields=None
-    ):
+    def delete_resource(self, path, permanently=None, md5=None, force_async=None, fields=None):
         params = {"path": path}
 
         if permanently is not None:
@@ -146,14 +121,12 @@ class YandexDiskClient:
         if fields is not None:
             params["fields"] = fields
 
-        response = requests.delete(
+        response = self.session.delete(
             f"{self.base_url}/v1/disk/resources",
-            headers=self.headers,
-            params=params
+            params=params,
+            timeout=self.timeout
         )
-
         self.log_response(response)
-
         return response
 
     def get_trash_resource_metadata(self, path="/", limit=None, offset=None):
@@ -164,23 +137,15 @@ class YandexDiskClient:
         if offset is not None:
             params["offset"] = offset
 
-        response = requests.get(
+        response = self.session.get(
             f"{self.base_url}/v1/disk/trash/resources",
-            headers=self.headers,
-            params=params
+            params=params,
+            timeout=self.timeout
         )
-
         self.log_response(response)
-
         return response
 
-    def restore_from_trash(
-        self,
-        path,
-        name=None,
-        overwrite=None,
-        fields=None
-    ):
+    def restore_from_trash(self, path, name=None, overwrite=None, fields=None):
         params = {"path": path}
 
         if name is not None:
@@ -190,28 +155,16 @@ class YandexDiskClient:
         if fields is not None:
             params["fields"] = fields
 
-        response = requests.put(
+        response = self.session.put(
             f"{self.base_url}/v1/disk/trash/resources/restore",
-            headers=self.headers,
-            params=params
+            params=params,
+            timeout=self.timeout
         )
-
         self.log_response(response)
-
         return response
 
-    def move_resource(
-        self,
-        from_path,
-        to_path,
-        overwrite=None,
-        force_async=None,
-        fields=None
-    ):
-        params = {
-            "from": from_path,
-            "path": to_path
-        }
+    def move_resource(self, from_path, to_path, overwrite=None, force_async=None, fields=None):
+        params = {"from": from_path, "path": to_path}
 
         if overwrite is not None:
             params["overwrite"] = str(overwrite).lower()
@@ -220,73 +173,60 @@ class YandexDiskClient:
         if fields is not None:
             params["fields"] = fields
 
-        response = requests.post(
+        response = self.session.post(
             f"{self.base_url}/v1/disk/resources/move",
-            headers=self.headers,
-            params=params
+            params=params,
+            timeout=self.timeout
         )
-
         self.log_response(response)
-
         return response
-    
+
     def get_upload_link(self, path, overwrite=False, fields=None):
         params = {
             "path": path,
             "overwrite": str(overwrite).lower()
         }
-
         if fields is not None:
             params["fields"] = fields
 
-        response = requests.get(
+        response = self.session.get(
             f"{self.base_url}/v1/disk/resources/upload",
-            headers=self.headers,
-            params=params
+            params=params,
+            timeout=self.timeout
         )
-
         self.log_response(response)
-
         return response
-    
+
     def upload_file(self, upload_url, file_path):
         with open(file_path, "rb") as file:
             response = requests.put(
                 upload_url,
-                files={"file": file}
+                files={"file": file},
+                timeout=self.timeout
             )
-
         self.log_response(response)
-
         return response
-    
+
     def get_download_link(self, path, fields=None):
         params = {"path": path}
-
         if fields is not None:
             params["fields"] = fields
 
-        response = requests.get(
+        response = self.session.get(
             f"{self.base_url}/v1/disk/resources/download",
-            headers=self.headers,
-            params=params
+            params=params,
+            timeout=self.timeout
         )
-
         self.log_response(response)
-
         return response
-
 
     def download_file(self, download_url):
-        response = requests.get(
+        response = self.session.get(
             download_url,
-            headers=self.headers
+            timeout=self.timeout
         )
-
         self.log_response(response)
-
         return response
-    
 
     def publish_resource(self, path, allow_address_access=None, fields=None):
         params = {"path": path}
@@ -296,47 +236,37 @@ class YandexDiskClient:
         if fields is not None:
             params["fields"] = fields
 
-        response = requests.put(
+        response = self.session.put(
             f"{self.base_url}/v1/disk/resources/publish",
-            headers=self.headers,
-            params=params
+            params=params,
+            timeout=self.timeout
         )
-
         self.log_response(response)
-
         return response
-
 
     def unpublish_resource(self, path, fields=None):
         params = {"path": path}
-
         if fields is not None:
             params["fields"] = fields
 
-        response = requests.put(
+        response = self.session.put(
             f"{self.base_url}/v1/disk/resources/unpublish",
-            headers=self.headers,
-            params=params
+            params=params,
+            timeout=self.timeout
         )
-
         self.log_response(response)
-
         return response
-    
 
     def update_resource_custom_properties(self, path, custom_properties, fields=None):
         params = {"path": path}
-
         if fields is not None:
             params["fields"] = fields
 
-        response = requests.patch(
+        response = self.session.patch(
             f"{self.base_url}/v1/disk/resources",
-            headers=self.headers,
             params=params,
-            json={"custom_properties": custom_properties}
+            json={"custom_properties": custom_properties},
+            timeout=self.timeout
         )
-
         self.log_response(response)
-
         return response
